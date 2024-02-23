cce14ce348e963ef4405064245e0f3d7a46ad139
ThilineTissera <thilinaknight2000kitt@gmail.com>
2024-02-23 19:55:47 +0530

    Thilina-update as instucted

--- src/main/java/com/techbirdssolutions/pos/PosApplication.java
+++ src/main/java/com/techbirdssolutions/pos/PosApplication.java
@@ -3,18 +3,15 @@ package com.techbirdssolutions.pos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.SQLOutput;
 
 @SpringBootApplication
-public class PosApplication
-{
+public class PosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PosApplication.class, args);
 	}
 
-	public void data()
-	{
+	public void data() {
 		String asd="Hellow World";
 		System.out.println(asd);
 	}
