c3c54e759bb35da6e328dcb6c687a9b94d4e05ee
ThilineTissera <thilinaknight2000kitt@gmail.com>
2024-02-23 19:31:23 +0530

    Thilina-Test -add enter

--- src/main/java/com/techbirdssolutions/pos/PosApplication.java
+++ src/main/java/com/techbirdssolutions/pos/PosApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PosApplication {
+public class PosApplication
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PosApplication.class, args);
