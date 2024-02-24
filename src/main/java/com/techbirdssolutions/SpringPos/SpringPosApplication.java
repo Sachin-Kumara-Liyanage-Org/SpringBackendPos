4fd49f2a2f8dfce1a74e7abbd35448c370eebdd2
Sachin Kumara Liyanage <sachinkumaraliyanage@gmail.com>
2024-02-24 17:19:27 +0530

    add error

--- src/main/java/com/techbirdssolutions/SpringPos/SpringPosApplication.java
+++ src/main/java/com/techbirdssolutions/SpringPos/SpringPosApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringPosApplication {
+public class SpringPosApplication 
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPosApplication.class, args);
